af940d6d12e82d1f3100edf61b79f24f6c1a1603
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-31 12:05:15 +0000

    messages object

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+from pprint import pprint
 
 token = '1623544581:AAGcmnpip3itggtxdUpX_GK7CdW-y1jmkic'
-url = f'https://api.telegram.org/bot{token}/getMe'
+
+url = f'https://api.telegram.org/bot{token}/getUpdates'
 
 r = requests.get(url)
-print(r.json())
+pprint(r.json()['result'][0]['message'])
