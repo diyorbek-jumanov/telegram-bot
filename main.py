47af4c4ec365864051cd280d87996fee2617744e
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-31 12:23:17 +0000

    full name

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
+import json
 from pprint import pprint
 
 token = '1623544581:AAGcmnpip3itggtxdUpX_GK7CdW-y1jmkic'
-
 url = f'https://api.telegram.org/bot{token}/getUpdates'
-
 r = requests.get(url)
-pprint(r.json()['result'][0]['message'])
+data = r.json()
+updates = data['result']
+
+for update in updates:
+    message = update['message']
+    user = message['from']
+    first_name = user.get('first_name', None)
+    last_name = user.get('last_name', None)
+    full_name = f'{first_name} {last_name}'
+    print(full_name)
